25fd20c1ad0a9d31038d62476b8b463da8c00e1e
Ubuntu <ubuntu@ip-172-31-22-15.us-east-2.compute.internal>
2024-02-04 20:36:14 +0000

    Implemented main program and comment

--- search_prefix.rb
+++ search_prefix.rb
@@ -1,7 +1,21 @@
-def searchPrefix(arr,s)
+# This program reads an array of strings and a
+# start string, then tells you which strings in
+# the array start with the start string
+# By Joel Coons, 2/4/2024
 
+def search_prefix(original_array, string_start)
+    new_array = []
+    end_range = string_start.length
+    original_array.each do |element|
+        if element[0...end_range] == string_start
+            new_array.push(element)
+        end
+    end
+    return new_array
 end
 
-print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
-
-print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
+print("Enter an array: ")
+my_array = gets.chomp.delete("[]\"").split(", ")
+print("Starts with: ")
+my_start = gets.chomp
+print(search_prefix(my_array, my_start), "\n")
